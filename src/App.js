c72a3d745bdb6ef836a9d7754e208382ef15a01d
Akwasi Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2022-11-07 11:19:58 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 function App () {
   return (
     <div className='h-full w-full'>
-      {/* <Navbar></Navbar> */}
+      <Navbar></Navbar>
       <Home></Home>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Navbar () {
   return (
-    <div className=' w-full h-24 absolute'>
+    <div className=' w-full h-24 absolute z-50'>
       <div className=' w-3/4 h-full mx-auto flex items-center justify-between relative'>
-        <div className='logo text-black text-xl'>Turvato</div>
+        <div className='logo text-white text-xl font-bold'>Turvato</div>
         {/* nav links */}
-        <ul className='flex gap-12'>
+        <ul className='flex gap-12 text-white'>
           <li className='links'>Home</li>
           <li>About</li>
           <li>Contact us</li>
         </ul>
-        <button className='border border-red-900 px-8 py-3 rounded-md'>
+        <button className='border border-white text-white px-8 py-3 rounded-md'>
           Sign in
         </button>
       </div>
